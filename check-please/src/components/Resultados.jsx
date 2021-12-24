2a20bed27e8bfdf0e9b5499cf0678db3b9ab49a5
ffranco-a <ffrancoaparicio@gmail.com>
2021-12-24 12:45:49 -0300

    merge new pc

--- check-please/src/components/Resultados.jsx
+++ check-please/src/components/Resultados.jsx
@@ -6,18 +6,37 @@ function Resultados({ resultados }) {
   ) : (
     <div className='flex flex-col'>
       <div>Gasto total = {resultados.total}</div>
-      <div>División tradicional = {resultados.divisionPartesIguales}</div>
+      <div>
+        División entre {resultados.grupo.cantidad} personas = {resultados.divisionPartesIguales}
+      </div>
+      <hr />
       {!resultados.todesCompartenTodo && (
         <div className='flex flex-col'>
-          Gastos por Categoría ={' '}
+          CATEGORIAS ={' '}
           {resultados.categorias?.map((categoria) => (
             <span key={categoria.id}>
-              <span className='capitalize'>{categoria.detalle}:</span> {categoria.monto.format()} , dividido entre <span title={`Compartieron: ${categoria.comparten.personas.join(', ')}`}>{categoria.comparten.cantidad}{' '}
-              personas</span> queda en {categoria.montoIndividual} cada una.
+              <span className='capitalize'>{categoria.detalle}:</span> {categoria.monto.format()} , dividido entre{' '}
+              <span title={`Compartieron: ${categoria.comparten.personas.join(', ')}`}>{categoria.comparten.cantidad} personas</span> queda en{' '}
+              {categoria.montoIndividual} cada una.
             </span>
           ))}
         </div>
       )}
+      <hr />
+      PERSONA POR PERSONA ={' '}
+      {resultados.grupo.personas.map((persona) => (
+        <div key={persona.id}>
+          <br />
+          <span className='capitalize font-bold'>{persona.nombre}</span> consumió un total de {persona.comparte.total}<br />
+          {persona.puso === '$0.00' ? (
+            <span> y como no puso nada <span className='font-bold'>ese es el monto que debe poner al fondo común ({persona.debe}).</span></span>
+          ) : persona.debe.includes('-') ? (
+            <span> pero como puso más que eso (puso {persona.puso} en total), <span className='font-bold'>debe sacar {persona.debe.substring(1)} del fondo común.</span></span>
+          ) : (
+            <span> pero como puso {persona.puso} <span className='font-bold'>debe poner solo {persona.debe} en el fondo común.</span></span>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
